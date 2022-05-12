82de20c3ce4ede7dd73e910100183bc0ad5a6710
nate <natasakotaranin1@gmail.com>
2022-05-12 23:59:52 +0200

    main

--- src/main/BibliotekaMain.java
+++ src/main/BibliotekaMain.java
@@ -1,6 +1,16 @@
 package main;
 
 public class BibliotekaMain {
-
-
+	private static String KNJIGE_FAJL = "knjige.txt";
+	private static String ZANROVI_FAJL = "zanroviKnjige.txt";
+	private static String CLANOVI_FAJL = "clanoviBiblioteke.txt";
+	private static String PRIMERCI_FAJL = "primerciKnjiga.txt";
+	private static String TIPOVI_FAJL = "tipoviClanarine.txt";
+	private static String ADMINISTRATORI_FAJL = "administratori.txt";
+	private static String BIBLIOTEKARI_FAJL = "bibliotekari.txt";
+	private static String IZNAJMLJIVANJE_FAJL = "izdavanjeKnjiga.txt";
+	
+	
 }
+
+
